2f19e6526ede3c1ec18b14f957aa18b9897b130a
xpfxz <xpfxz@bk.ru>
2025-03-11 20:37:06 +0300

    добавлен асинхронный пул

--- src/app/worker.py
+++ src/app/worker.py
@@ -1,4 +1,5 @@
 import os
+from dotenv import load_dotenv
 from datetime import datetime, timezone
 from celery import Celery, shared_task
 from celery.schedules import crontab
@@ -9,13 +10,20 @@ from fastapi import Depends
 from app.db import get_session, engine
 from app.models.auth import TokenModel
 
+import logging
 
-celery = Celery(__name__)
+logger = logging.getLogger(__name__)
+
+load_dotenv()
+
+celery = Celery('worker')
 celery.conf.broker_url = os.environ.get("BROKER_URL")
 celery.conf.result_backend = os.environ.get("BROKER_URL")
+celery.conf.worker_pool = 'eventlet' # использования асинхронного пула, потому что селери ожидает синхронные функции
 
 @celery.task(name="cleanup_expired_refresh_tokens")
 async def cleanup_expired_refresh_tokens():
+    logger.info("Задача cleanup_expired_refresh_tokens запущена.")
     """Удаляет из базы данных просроченные и недействительные токены."""
     async with AsyncSession(engine) as db:
         
@@ -31,10 +39,12 @@ async def cleanup_expired_refresh_tokens():
             await db.delete(token) 
 
         await db.commit()
+    return None
 
-celery.conf.beat_shedule = {
+celery.conf.beat_schedule = {
     'cleanup_tokens': {
         'task': 'cleanup_expired_refresh_tokens',
-        'schedule': crontab(minute=0, hour='*/24'),
+        # 'schedule': crontab(minute=0, hour='*/24'),
+        'schedule': crontab(minute='*/1')
     }
 }
