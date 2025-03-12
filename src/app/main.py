937e13114425b88e47abb7eb214ba68d7bbd8a91
xpfxz <xpfxz@bk.ru>
2025-03-12 16:18:13 +0300

    использование asyncio задач вместо celery

--- src/app/main.py
+++ src/app/main.py
@@ -1,18 +1,38 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
-
+from contextlib import asynccontextmanager
+from app.worker import cleanup_expired_refresh_tokens
+import asyncio
 from app.db import init_db, engine
 from app.models.auth import UserAuthModel
 from app.routers.auth import router as auth_router
+
+# вызов функции для очистки невалидных токенов каждые 24 часа
+async def run_periodically():
+    while True:
+        await cleanup_expired_refresh_tokens()
+        await asyncio.sleep(60*60*24)
 
 # инициализация базы данных
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     await init_db(engine)
+    task = asyncio.create_task(run_periodically())
     yield
 
+    task.cancel()
+    try:
+        await task
+    except asyncio.CancelledError:
+        print("Периодическая задача остановлена.")
+
 app = FastAPI(lifespan=lifespan)
+
+async def run_periodically():
+    while True:
+        await cleanup_expired_refresh_tokens()
+        await asyncio.sleep(60)  # Запускать каждую минуту
 
 # настройка CORS
 app.add_middleware(
--- src/app/worker.py
+++ src/app/worker.py
@@ -1,4 +1,5 @@
 import os
+from rq_scheduler import Scheduler
 from dotenv import load_dotenv
 from datetime import datetime, timezone
 from celery import Celery, shared_task
@@ -6,45 +7,46 @@ from celery.schedules import crontab
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from fastapi import Depends
-
 from app.db import get_session, engine
 from app.models.auth import TokenModel
+from rq import scheduler
+import sys
+import asyncio
 
 import logging
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+console_handler = logging.StreamHandler(sys.stdout)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
 
 load_dotenv()
 
-celery = Celery('worker')
-celery.conf.broker_url = os.environ.get("BROKER_URL")
-celery.conf.result_backend = os.environ.get("BROKER_URL")
-celery.conf.worker_pool = 'eventlet' # использования асинхронного пула, потому что селери ожидает синхронные функции
-
-@celery.task(name="cleanup_expired_refresh_tokens")
 async def cleanup_expired_refresh_tokens():
     logger.info("Задача cleanup_expired_refresh_tokens запущена.")
     """Удаляет из базы данных просроченные и недействительные токены."""
-    async with AsyncSession(engine) as db:
-        
-        result = await db.execute(
-            select(TokenModel).where(
-                (TokenModel.expires_at < datetime.now(timezone.utc))
-                | (TokenModel.invalidated == True)
-            )
-        )
-        expired_tokens = result.scalars().all() 
+    try:
+        async with AsyncSession(engine) as db:
+            try:
+                # Выполняем запрос для поиска истекших токенов
+                result = await db.execute(
+                    select(TokenModel).where(
+                        TokenModel.invalidated == True
+                    )
+                )
+                expired_tokens = result.scalars().all() 
 
-        for token in expired_tokens:
-            await db.delete(token) 
+                # Удаляем каждый истекший токен
+                for token in expired_tokens:
+                    await db.delete(token) 
 
-        await db.commit()
-    return None
-
-celery.conf.beat_schedule = {
-    'cleanup_tokens': {
-        'task': 'cleanup_expired_refresh_tokens',
-        # 'schedule': crontab(minute=0, hour='*/24'),
-        'schedule': crontab(minute='*/1')
-    }
-}
+                # Фиксируем транзакцию
+                await db.commit()
+            except Exception as e:
+                # Откатываем транзакцию в случае ошибки
+                await db.rollback()
+                print(f"Error in cleanup_expired_refresh_tokens: {e}")
+    except Exception as e:
+        print(f"Error with session: {e}")
